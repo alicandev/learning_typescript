d9d740b8ac96cb507da10e33a51f88d588816e9d
Alican Demirtas <alican@compositional-it.com>
2020-04-30 08:07:45 +0100

    Experimented with some ES6 features such as default parameters, string templates, `let` and `const` keywords, arrow functions, destructuring.

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-var todo = "Pick up drycleaning";
-var myName = "Alican";
+var _a;
+var CONTAINER = document.querySelector('.container');
+// let todo = {
+//     id: 123,
+//     name: "Pick up dry cleaning",
+//     completed: true
+// }
+// CONTAINER.innerHTML = `
+//     <div todo='${ todo.id }' class="list-group-item}">
+//         <i class="${ todo.completed ? "" : "hidden" } text-success glyphicon glyphicon-ok"></i>
+//         <span class="name">${ todo.name }</span> 
+//     </div>
+// `
+// let array = [ "Pick up dry-cleaning", "Clean Bat Cave", "Save Gotham" ];
+// for (let value of array) { console.log(`${value}`) }
+// arrow functions
+function Counter(el) {
+    var _this = this;
+    this.count = 0;
+    el.innerHtml = "<h1>" + this.count + "</h1>";
+    el.addEventListener('click', function () {
+        _this.count += 1;
+        el.innerHTML = _this.count;
+    });
+}
+new Counter(CONTAINER);
+var filtered = [1, 2, 3].filter(function (x) { return x > 0; });
+//destructuring
+var array = [123, "Pick up dry cleaning.", false];
+var id = array[0], title = array[1], completed = array[2];
+console.log("ID:" + id);
+var a = 1;
+var b = 5;
+_a = [b, a], a = _a[0], b = _a[1];
--- app.ts
+++ app.ts
@@ -1,2 +1,44 @@
-﻿let todo : string = "Pick up drycleaning"
-let myName : string = "Alican"
+﻿const CONTAINER = document.querySelector('.container');
+
+// let todo = {
+//     id: 123,
+//     name: "Pick up dry cleaning",
+//     completed: true
+// }
+
+// CONTAINER.innerHTML = `
+//     <div todo='${ todo.id }' class="list-group-item}">
+//         <i class="${ todo.completed ? "" : "hidden" } text-success glyphicon glyphicon-ok"></i>
+//         <span class="name">${ todo.name }</span> 
+//     </div>
+// `
+
+// let array = [ "Pick up dry-cleaning", "Clean Bat Cave", "Save Gotham" ];
+// for (let value of array) { console.log(`${value}`) }
+
+// arrow functions
+function Counter(el) {
+    this.count = 0;
+    
+    el.innerHtml = `<h1>${ this.count }</h1>`;
+    
+    el.addEventListener('click',
+        () => {
+            this.count += 1;
+            el.innerHTML = this.count;
+        });
+}
+
+new Counter(CONTAINER);
+
+let filtered = [1, 2, 3].filter(x => x > 0);
+
+//destructuring
+let array = [123, "Pick up dry cleaning.", false];
+let [id, title, completed] = array;
+console.log(`ID:${id}`);
+
+let a = 1;
+let b = 5;
+
+[a,b] = [b,a];
